ac6790204541ef96b22a4d3089fd73c1a9183bf6
Matt <mmccann@ssolutions.ca>
2016-03-28 17:06:40 -0400

    First Commit
    Hacky buy gets the job done

---
+++ CreateSketchImageXcassets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSketchImageXcassets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the image folder");
+
+            var folderlocation = Console.ReadLine();
+            Console.WriteLine("enter the image Direction");
+            var Direction = Console.ReadLine();
+            Console.WriteLine("enter the image Layer Name");
+            var Layer = Console.ReadLine();
+
+            DirectoryInfo dirInfo = new DirectoryInfo(folderlocation);
+            //create new directory
+          //  DirectoryInfo newdir = Directory.CreateDirectory(folderlocation + "\\" + dirInfo.GetFiles()[0].Name.Replace(".png",""));
+        
+            foreach (FileInfo f in dirInfo.GetFiles())
+            {
+                if (f.Name.Contains(".png"))
+                {
+                    //create the image set folder
+                    DirectoryInfo imgsetfolder = Directory.CreateDirectory(f.FullName.Replace(".png", ".imageset").Replace("N",Direction).Replace("Skin",Layer));
+                    //copy file to directory
+                    f.CopyTo(imgsetfolder.FullName + "\\" + f.Name.Replace("N",Direction).Replace("Skin",Layer));
+                    //create jsonfile in folder
+                    string jsontext = GetFileText().Replace("FemaleSkinN2.png", f.Name.Replace("N",Direction).Replace("Skin", Layer));
+                    using (StreamWriter sw = new StreamWriter(imgsetfolder.FullName + "//" + "Contents.json"))
+                    {
+                        sw.Write(jsontext);
+                    }
+
+                    f.Delete();
+
+                }
+            }
+        }
+        public static string GetFileText()
+        {
+            StreamReader sw = new StreamReader("Contents.json");
+            string file = sw.ReadToEnd();
+            return file;
+        }
+
+    }
+}
